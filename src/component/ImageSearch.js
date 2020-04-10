34a804c722aed6a1b86f951449efcbe1359b4161
pirmansh <fsyah1362@gmail.com>
2020-04-10 12:49:43 +0700

    oke

--- src/component/ImageSearch.js
+++ src/component/ImageSearch.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function ImageSearch() {
+const ImageSearch = ({ searchText }) => {
+    const [text, setText] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+        searchText(text);
+    }
+
     return (
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
-            <form className="w-full max-w-sm">
+            <form onSubmit={onSubmit} className="w-full max-w-sm">
                 <div className="flex items-center border-b border-b-2 border-blue-500 py-2">
-                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image ..." />
+                    <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image ..." />
                     <button className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
                     </button>
